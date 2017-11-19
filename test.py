e241a67773683124ff8103738a4f266fb375faa9
mjyousif <33187058+mjyousif@users.noreply.github.com>
2017-11-19 03:05:02 -0500

    Added new operations, tidied up
    Added market cap, 1hr pct change, 24hr pct change, 7day pct change. Tidied up the json_api.py, should be more straight forward

---
+++ test.py
@@ -0,0 +1,19 @@
+# from json_api import *
+# import decimal
+# x=getCoinData('btc')
+# print(float(x['price_usd']))
+# print(oneHourChange('doge'))
+# string="123.4567"
+# print (string)
+# y=(string.split('.',1))
+# print (len(y[1]))
+# print ("%f" % 1.2399)
+
+
+#-------------------------------------------------
+# ,	
+		 # InlineQueryResultArticle(
+             # id=uuid4(),
+             # title='Summary',
+             # input_message_content=InputTextMessageContent(summary(query))
+         # )	
