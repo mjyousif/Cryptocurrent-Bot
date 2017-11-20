527b51f77ad48b32bbc98b4158f30e09f68b4a5f
mjyousif <33187058+mjyousif@users.noreply.github.com>
2017-11-20 00:26:23 -0500

    Added better formatting and more shorthands
    prettyANDprecise to format dollar values with commas. Changed the coinmarketcap ticker to get all coins rather than the first 100

--- test.py
+++ test.py
@@ -11,9 +11,19 @@
 
 
 #-------------------------------------------------
-# ,	
-		 # InlineQueryResultArticle(
-             # id=uuid4(),
-             # title='Summary',
-             # input_message_content=InputTextMessageContent(summary(query))
-         # )	
+from json_api import *
+# # numStr='2181.120'
+# # numStrSplit=(numStr.split('.',1))
+# # numStrFormatLeft="{:,}".format(float(numStrSplit[0]))
+# # numStrFormatLeft=(numStrFormatLeft.split('.',1))
+# # numStrFormat=(str(numStrFormatLeft[0])+'.'+numStrSplit[1])
+# # print (numStrFormat)
+# print(prettyANDprecise('2183.1200'))
+#------------------------------
+# print(getCoinData('ltc'))
+# print(prettyANDprecise('2183'))
+# print('------')
+# print(prettyANDprecise('2183.120'))
+print(getCoinData('bitcoin'))
+print(getCoinData('ltc'))
+print(getCoinData('ponzi'))
