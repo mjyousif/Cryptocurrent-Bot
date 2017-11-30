480d678d72e956632b91194884cfb9f18c6f131b
mjyousif <33187058+mjyousif@users.noreply.github.com>
2017-11-29 20:25:23 -0500

    Update CryptoCurrent_Bot to V2
    Massive overhaul. Simplified functions in json_api, pass in the coinData rather than each getting its data on its own. Updated the crypto currency menu to show values in the menu and added icons. Added feedReader to introduce the "news" functionality as well as "news <tag>" ability to filter news to certain tags. app.py changed to reflect these abilities.

--- app.py
+++ app.py
@@ -3,84 +3,105 @@ from telegram import InlineQueryResultArticle, InlineQueryResultPhoto, InputText
 
 from uuid import uuid4
 from json_api import *
-
-
+from feedReader import *
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(bot, update):
-	bot.send_message(
-					chat_id=update.message.chat_id, 
-					text=('Use me inline by tagging me and typing a crypto currency!')
-					)
-	
+    bot.send_message(
+                    chat_id=update.message.chat_id, 
+                    text=('Use me inline by tagging me and typing a crypto currency!')
+                    )
+    
 def inline_crypto(bot, update):
-     query = update.inline_query.query
-     if not query:
-         return
-     results = [
-		 InlineQueryResultPhoto(
-             id=uuid4(),
-			 photo_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+getCoinID(query) +'.png'),
-			 thumb_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+getCoinID(query) +'.png'),
-             thumb_width=512,
-			 # title='Summary2'#,
-             # caption=summary(query)
-		 ),
-         InlineQueryResultArticle(
-             id=uuid4(),
-             title='USD Value',
-             input_message_content=InputTextMessageContent(cryptoValueMain(query)),
-			 thumb_url=None,
-			 #thumb_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+getCoinID(query) +'.png')
-         ),
-		 InlineQueryResultArticle(
-             id=uuid4(),
-             title='Market Capitalization',
-             input_message_content=InputTextMessageContent(cryptoMarketCap(query))
-         ),
-		 InlineQueryResultArticle(
-             id=uuid4(),
-             title='1 hour change',
-             input_message_content=InputTextMessageContent(oneHourChange(query))
-         ),
-		 InlineQueryResultArticle(
-             id=uuid4(),
-             title='24 hour change',
-             input_message_content=InputTextMessageContent(oneDayChange(query))
-         ),
-		 InlineQueryResultArticle(
-             id=uuid4(),
-             title='7 day change',
-             input_message_content=InputTextMessageContent(sevenDayChange(query))
-         ),	
-		 InlineQueryResultArticle(
-             id=uuid4(),
-             title='Summary',
-             input_message_content=InputTextMessageContent(summary(query))
-         )
+    query = update.inline_query.query
+    if not query:
+        return
+    newsQuery=query.split(" ")
+    if (newsQuery[0].lower()=="news"):
+        if (len(newsQuery)==1):
+            newsArticles=news()
+        else:
+            # newsArticles=news(newsQuery[1])
+            j=0
+            for j in (range(len(newsQuery)-2)):
+                newsQuery[1]=newsQuery[1]+' '+newsQuery[2]
+                del newsQuery[2]
+            newsArticles=news(newsQuery[1])
+        # newsArticles=news(None if len(newsQuery)==1 else newsQuery[1])
+        results=[]
+        i=0
+        for i in range(len(newsArticles) if len(newsArticles)<5 else 5):
+            results.append(InlineQueryResultArticle(id=uuid4(),title=newsArticles[i].title,input_message_content=InputTextMessageContent(newsArticles[i].link),description=newsArticles[i].description,))
+    else:
+        querySplitted=querySplit(query)
+        coinData=getCoinData(query)
+        coinName=coinData['name']
+        results = [
+            InlineQueryResultPhoto(
+                id=uuid4(),
+                photo_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+coinData['id'] +'.png'),
+                thumb_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+coinData['id'] +'.png'),
+                title=coinData['name']+'('+coinData['symbol']+')',
+                caption=coinData['name']+' ('+coinData['symbol']+')',
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title='Value: '+cryptoValueMain(querySplitted, coinData),
+                input_message_content=InputTextMessageContent(coinName+': '+cryptoValueMain(querySplitted, coinData)),
+                thumb_url='https://i.imgur.com/My7IG7r.png'
+                #thumb_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+getCoinID(query) +'.png')
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title='Market Capitalization: '+cryptoMarketCap(querySplitted, coinData),
+                input_message_content=InputTextMessageContent(coinName+': '+cryptoMarketCap(querySplitted, coinData)),
+                thumb_url='https://i.imgur.com/egncB1b.png'
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title=oneHourChange(coinData),
+                input_message_content=InputTextMessageContent(coinName+': '+oneHourChange(coinData)),
+                thumb_url='https://i.imgur.com/pza5Xjb.png'
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title=oneDayChange(coinData),
+                input_message_content=InputTextMessageContent(coinName+': '+oneDayChange(coinData)),
+                thumb_url='https://i.imgur.com/98YM0PA.png'
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title=sevenDayChange(coinData),
+                input_message_content=InputTextMessageContent(coinName+': '+sevenDayChange(coinData)),
+                thumb_url='https://i.imgur.com/ZbPOM53.png'
+            ),    
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title='Summary',
+                input_message_content=InputTextMessageContent(summary(querySplitted, coinData)),
+                thumb_url='https://i.imgur.com/t6BPcMR.png'
+            )
+        ]
+    bot.answer_inline_query(update.inline_query.id, results)    
 
-     ]
-     bot.answer_inline_query(update.inline_query.id, results)	
-
-	 
+     
 def main():
-	updater = Updater(token='460892339:AAGDPHRipffdOM8AR7IXdsUoMpnIsoSJwFw')
-	dp=updater.dispatcher
-	
-	#commands to answer
-	dp.add_handler(CommandHandler('start', start))
-	
-	#non commands
-	dp.add_handler(InlineQueryHandler(inline_crypto))
+    updater = Updater(token='491978101:AAEM4E-DMKblOLwxvbkO6XOMhoXgwRATvVs')
+    dp=updater.dispatcher
+    
+    #commands to answer
+    dp.add_handler(CommandHandler('start', start))
+    
+    #non commands
+    dp.add_handler(InlineQueryHandler(inline_crypto))
 
+    #start bot
 
-
-	#start bot
-	updater.start_polling()
-	#close with ctrl-c
-	updater.idle()
-	
+    updater.start_polling()
+    #close with ctrl-c
+    updater.idle()
+    
 if __name__=='__main__':
-	main()
+    main()
--- json_api.py
+++ json_api.py
@@ -1,8 +1,7 @@
 import requests
 #------------------------------------------------------------------
-#Misc. functions for use here
 #comma separated numbers that are entered as strings
-#keeps precision of the numbers
+#keeps precision of the numbers as shown in the coinmarketcap api in usd
 def prettyANDprecise(strNum):
 	numStrSplit=(strNum.split('.',1))
 	numStrFormatLeft="{:,}".format(float(numStrSplit[0]))
@@ -12,16 +11,45 @@ def prettyANDprecise(strNum):
 	else:
 		pApOutput=(str(numStrFormatLeft[0])+'.'+numStrSplit[1])
 	return pApOutput
-
+	
+def queryLength(query):
+	queryList=query.split(" ")
+	queryLength=len(queryList)
+	return queryLength
+	
+def querySplit(query):
+	acceptedCurrencies=["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR", "USD"]
+	
+	querySplit=query.split(" ")
+	#loop to see if there is a currency, or if needs the default
+	i=0
+	defaultCurrency=True
+	for i in range(len(acceptedCurrencies)):
+		if(querySplit[-1].upper()==acceptedCurrencies[i]):
+			defaultCurrency=False
+	if defaultCurrency==True:
+		querySplit.append('usd')
+	#the output should always be 2 indices, one for the coin, the other for the currency
+	#if there are more than two, then the coin is multiple words
+	while(len(querySplit)>2):
+		querySplit[0]=querySplit[0]+'-'+querySplit[1]
+		del querySplit[1]
+		
+	#todo, make sure 2nd word is currency| append the currency symbol
+	return querySplit
 #-------------------------------------------------------------------
 #Function to get the json for the coin
-def getCoinData(coin):
+def getCoinData(query):
+	if (query=="" or query==" "):
+		return
+	splitQuery=querySplit(query)
 	main_api='https://api.coinmarketcap.com/v1/ticker/'
-	coin=coin.replace(" ","-")
+	coin=splitQuery[0].replace(" ","-")
 	url=main_api+coin
+	url=url+'/?convert='+splitQuery[1]
 	json_data_end=requests.get(url).json() 
 	if 'error' in json_data_end: #This will check if the entered name exists, if not, check if the symbol exists
-		url='https://api.coinmarketcap.com/v1/ticker/?limit=10000'
+		url='https://api.coinmarketcap.com/v1/ticker/?limit=10000'+'&convert='+splitQuery[1]
 		json_data=requests.get(url).json()
 		index=0
 		for index in range(len(json_data)): #loops through entire coinmarketcap ticker until symbol found
@@ -37,28 +65,34 @@ def getCoinData(coin):
 	except:
 		json_data_end=json_data_end
 		
-
 	#this gets values with comma separations and makes sure that the data exists. if there is null data, replace with "N/A"
-	if (json_data_end['price_usd']==None):
-		json_data_end['price_usd']='N/A'
-	else:
-		json_data_end['price_usd']=prettyANDprecise(json_data_end['price_usd'])
+	currentPrice='price_'+splitQuery[1]
+	currentMarketCap='market_cap_'+splitQuery[1]
+	currentVolume='24h_volume_'+splitQuery[1]
 	
-	if (json_data_end['market_cap_usd']==None):
-		json_data_end['market_cap_usd']='N/A'
+	if (json_data_end[currentPrice]==None):
+		json_data_end[currentPrice]='N/A'
 	else:
-		json_data_end['market_cap_usd']=prettyANDprecise(json_data_end['market_cap_usd'])
+		json_data_end[currentPrice]=prettyANDprecise(json_data_end[currentPrice])
+		if (currentPrice!='price_usd'): #set precision to that of the price_usd
+			USDdivide=json_data_end['price_usd'].split(".")
+			precision=len(USDdivide[1])
+			currencyDivide=json_data_end[currentPrice].split(".")
+			json_data_end[currentPrice]=currencyDivide[0]+'.'+currencyDivide[1][0:precision]
+	
+	if (json_data_end[currentMarketCap]==None):
+		json_data_end[currentMarketCap]='N/A'
+	else:
+		json_data_end[currentMarketCap]=prettyANDprecise(json_data_end[currentMarketCap])
 	
 	#coinmarketcap info has a decimal at the end of market cap for some reason. this gets rid of that
 	json_data_end['market_cap_usd']=json_data_end['market_cap_usd'].split('.')
 	json_data_end['market_cap_usd']=json_data_end['market_cap_usd'][0]
-		
-		
-		
-	if (json_data_end['24h_volume_usd']==None):
-		json_data_end['24h_volume_usd']='N/A'
+				
+	if (json_data_end[currentVolume]==None):
+		json_data_end[currentVolume]='N/A'
 	else:		
-		json_data_end['24h_volume_usd']=prettyANDprecise(json_data_end['24h_volume_usd'])
+		json_data_end[currentVolume]=prettyANDprecise(json_data_end[currentVolume])
 	
 	if(json_data_end['percent_change_1h']==None):
 		json_data_end['percent_change_1h']='?'
@@ -68,73 +102,60 @@ def getCoinData(coin):
 		json_data_end['percent_change_7d']='?'
 	
 	return json_data_end
-	
-	#function to get coin id
-def getCoinID(coin):
-	coinData=getCoinData(coin)
-	coinID=coinData['id']
-	return coinID
-
-
+    
 #----------------------------------------------------------------------
 
 #Function to get market cap
-def cryptoMarketCap(coin):
-	coinData=getCoinData(coin)
-	name=coinData['name']
-	marketCap=coinData['market_cap_usd']	
-	marketCapOutput=(name+' market capitalization: $'+marketCap)
+def cryptoMarketCap(querySplit, coinData):
+	# name=coinData['name']
+	marketCap=coinData['market_cap_'+querySplit[1]]	
+	marketCapOutput=marketCap+' '+querySplit[1].upper()
 	return marketCapOutput
 
 	#Function to get crypto value
-def cryptoValueMain(coin):
-	coinData=getCoinData(coin)
-	name=coinData['name']
-	priceUSD=coinData['price_usd']		
-	valueOutput=(name+': $'+priceUSD)
+def cryptoValueMain(querySplit, coinData):
+	# name=coinData['name']
+	price=coinData['price_'+querySplit[1]]		
+	valueOutput=price+' '+querySplit[1].upper()
 	return valueOutput
 
 	#function gets percent change in 1 hour
-def oneHourChange(coin):
-	coinData=getCoinData(coin)
-	name=coinData['name']
-	priceUSD=coinData['price_usd']	
+def oneHourChange(coinData):
+	# name=coinData['name']
 	pctChange1hr=coinData['percent_change_1h']
-#	dollarChange1hr=float(priceUSD)/(1+float(pctChange1hr)/100)
-	oneHourChangeOutput=(name+" 1 hour percent change: "+pctChange1hr+"%")
+	oneHourChangeOutput="1 hour percent change: "+pctChange1hr+"%"
 	return oneHourChangeOutput
 
 	#function gets percent change in 24 hour
-def oneDayChange(coin):
-	coinData=getCoinData(coin)
-	name=coinData['name']
-	priceUSD=coinData['price_usd']	
-	pctChange24hr=coinData['percent_change_24h']
-	oneDayChangeOutput=(name+" 24 hour percent change: "+pctChange24hr+"%")	
-	return oneDayChangeOutput
+def oneDayChange(coinData):
+	# name=coinData['name']
+    # priceUSD=coinData['price_usd']	
+    pctChange24hr=coinData['percent_change_24h']
+    oneDayChangeOutput="24 hour percent change: "+pctChange24hr+"%"
+    return oneDayChangeOutput
 
 	#function gets percent change in 7 days
-def sevenDayChange(coin):
-	coinData=getCoinData(coin)
-	name=coinData['name']
-	priceUSD=coinData['price_usd']	
+def sevenDayChange(coinData):
+	# name=coinData['name']	
 	pctChange7d=coinData['percent_change_7d']
-	sevenDayChangeOutput=(name+" 7 day percent change: "+pctChange7d+"%")		
+	sevenDayChangeOutput="7 day percent change: "+pctChange7d+"%"
 	return sevenDayChangeOutput
 	
 	#function presents all all the above data in one string
-def summary(coin):
-	coinData=getCoinData(coin)
+def summary(querySplit, coinData):
 	name=coinData['name']
-	marketCap=coinData['market_cap_usd']
-	priceUSD=coinData['price_usd']	
+	marketCap=cryptoMarketCap(querySplit,coinData)
+	price=cryptoValueMain(querySplit,coinData)
 	pctChange1hr=coinData['percent_change_1h']
 	pctChange24hr=coinData['percent_change_24h']
 	pctChange7d=coinData['percent_change_7d']
 	summaryOutput=("---"+name+" Summary---"+
-		"\nPrice USD: $"+priceUSD+
-		"\nMarket Capitalization: $"+marketCap+
+		"\nPrice: "+price+' '+querySplit[1].upper()+
+		"\nMarket Capitalization: "+marketCap+
 		"\n1 hour percent change: "+pctChange1hr+"%"+
 		"\n24 hour percent change: "+pctChange24hr+"%"+
 		"\n7 day percent change: "+pctChange7d+"%")
 	return summaryOutput
+#---------------------------------------------------
+
+	
