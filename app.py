862e140f13a06b37f61c9ee2b4f682812b068d1c
mjyousif <33187058+mjyousif@users.noreply.github.com>
2018-01-01 15:49:33 -0500

    MultiCoins update
    Added ability to ask for multiple coins in a comma separated list. Minor changes elsewhere, including use of class to store coin and currency and modify API link

--- app.py
+++ app.py
@@ -34,55 +34,136 @@ def inline_crypto(bot, update):
         i=0
         for i in range(len(newsArticles)):
             results.append(InlineQueryResultArticle(id=uuid4(),title=newsArticles[i].title,input_message_content=InputTextMessageContent(newsArticles[i].link),description=newsArticles[i].description,))
+    if ',' in query:
+        classifiedQuery=classifyQuery(query)
+        i=0
+        jsonDataList=[]
+        #this loop saves all the data in a list to be used in parallel to the coinQuery list
+        for i in range(len(classifiedQuery.coinQuery)):
+            jsonDataList.append(getCoinData(classifiedQuery.coinQuery[i],classifiedQuery.currency))
+        #stuff that will go in the results, prepared up here because I can't do it in their respective results
+        nameList=""
+        k=0
+        for k in range(len(classifiedQuery.coinQuery)):
+            nameList+=jsonDataList[k]['name']+" ("+jsonDataList[k]['symbol']+"), \n"  
+        valueList="Values:\n"
+        k=0
+        for k in range(len(classifiedQuery.coinQuery)):
+            valueList+=jsonDataList[k]['name']+" ("+jsonDataList[k]['symbol']+"): "+jsonDataList[k]['price_'+classifiedQuery.currency.lower()]+" "+classifiedQuery.currency.upper()+"\n" 
+        capList="Market Caps:\n"
+        k=0
+        for k in range(len(classifiedQuery.coinQuery)):
+            capList+=jsonDataList[k]['name']+" ("+jsonDataList[k]['symbol']+"): "+jsonDataList[k]['market_cap_'+classifiedQuery.currency.lower()]+" "+classifiedQuery.currency.upper()+"\n" 
+        hourList="1 Hour Changes:\n"
+        k=0
+        for k in range(len(classifiedQuery.coinQuery)):
+            hourList+=jsonDataList[k]['name']+" ("+jsonDataList[k]['symbol']+"): "+jsonDataList[k]['percent_change_1h']+"%"+"\n"  
+        dayList="1 Day Changes:\n"
+        k=0
+        for k in range(len(classifiedQuery.coinQuery)):
+            dayList+=jsonDataList[k]['name']+" ("+jsonDataList[k]['symbol']+"): "+jsonDataList[k]['percent_change_24h']+"%"+"\n"  
+        weekList="7 Day Changes:\n"
+        k=0
+        for k in range(len(classifiedQuery.coinQuery)):
+            weekList+=jsonDataList[k]['name']+" ("+jsonDataList[k]['symbol']+"): "+jsonDataList[k]['percent_change_7d']+"%"+"\n"           
+        results = [
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title=nameList,
+                input_message_content=InputTextMessageContent(nameList),
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title='Values',
+                input_message_content=InputTextMessageContent(valueList),
+                thumb_url='https://i.imgur.com/My7IG7r.png'
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title='Market Capitalizations',
+                input_message_content=InputTextMessageContent(capList),
+                thumb_url='https://i.imgur.com/egncB1b.png'
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title="One Hour Changes",
+                input_message_content=InputTextMessageContent(hourList),
+                thumb_url='https://i.imgur.com/pza5Xjb.png'
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title="One Day Changes",
+                input_message_content=InputTextMessageContent(dayList),
+                thumb_url='https://i.imgur.com/98YM0PA.png'
+            ),
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title="Seven Day Changes",
+                input_message_content=InputTextMessageContent(weekList),
+                thumb_url='https://i.imgur.com/ZbPOM53.png'
+            ),   
+        ]
+    
     else:
-        querySplitted=querySplit(query)
-        coinData=getCoinData(query)
+        classifiedQuery=classifyQuery(query)
+        coinData=getCoinData(classifiedQuery.coinQuery[0],classifiedQuery.currency)
         coinName=coinData['name']
+        coinSymbol=coinData['symbol']
+        coinPrice=coinData['price_'+classifiedQuery.currency]+" "+classifiedQuery.currency.upper()
+        coinCap=coinData['market_cap_'+classifiedQuery.currency]+" "+classifiedQuery.currency.upper()
+        coin1hr=coinData['percent_change_1h']+"%"
+        coin1day=coinData['percent_change_24h']+"%"
+        coin7day=coinData['percent_change_7d']+"%"
         results = [
             InlineQueryResultPhoto(
                 id=uuid4(),
                 photo_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+coinData['id'] +'.png'),
                 thumb_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+coinData['id'] +'.png'),
-                title=coinData['name']+'('+coinData['symbol']+')',
-                caption=coinData['name']+' ('+coinData['symbol']+')',
+                title=coinData['name']+'('+coinSymbol+')',
+                caption=coinData['name']+' ('+coinSymbol+')',
             ),
             InlineQueryResultArticle(
                 id=uuid4(),
-                title='Value: '+cryptoValueMain(querySplitted, coinData),
-                input_message_content=InputTextMessageContent(coinName+': '+cryptoValueMain(querySplitted, coinData)),
+                title='Value: '+coinPrice,
+                input_message_content=InputTextMessageContent(coinName+': '+coinPrice),
                 thumb_url='https://i.imgur.com/My7IG7r.png'
-                #thumb_url=('https://files.coinmarketcap.com/static/img/coins/128x128/'+getCoinID(query) +'.png')
             ),
             InlineQueryResultArticle(
                 id=uuid4(),
-                title='Market Capitalization: '+cryptoMarketCap(querySplitted, coinData),
-                input_message_content=InputTextMessageContent(coinName+' Market Capitalization: '+cryptoMarketCap(querySplitted, coinData)),
+                title='Market Capitalization: '+coinCap,
+                input_message_content=InputTextMessageContent(coinName+' Market Capitalization: '+coinCap),
                 thumb_url='https://i.imgur.com/egncB1b.png'
             ),
             InlineQueryResultArticle(
                 id=uuid4(),
-                title=oneHourChange(coinData),
-                input_message_content=InputTextMessageContent(coinName+' '+oneHourChange(coinData)),
+                title="One Hour Change: "+coin1hr,
+                input_message_content=InputTextMessageContent(coinName+' One Hour Change: '+coin1hr),
                 thumb_url='https://i.imgur.com/pza5Xjb.png'
             ),
             InlineQueryResultArticle(
                 id=uuid4(),
-                title=oneDayChange(coinData),
-                input_message_content=InputTextMessageContent(coinName+' '+oneDayChange(coinData)),
+                title="One Day Change: "+coin1day,
+                input_message_content=InputTextMessageContent(coinName+' One Day Change: '+coin1day),
                 thumb_url='https://i.imgur.com/98YM0PA.png'
             ),
             InlineQueryResultArticle(
                 id=uuid4(),
-                title=sevenDayChange(coinData),
-                input_message_content=InputTextMessageContent(coinName+' '+sevenDayChange(coinData)),
+                title="Seven Day Change: "+coin7day,
+                input_message_content=InputTextMessageContent(coinName+' Seven Day Change: '+coin7day),
                 thumb_url='https://i.imgur.com/ZbPOM53.png'
             ),    
             InlineQueryResultArticle(
                 id=uuid4(),
-                title='Summary of '+coinName+'('+coinData['symbol']+')',
-                input_message_content=InputTextMessageContent(summary(querySplitted, coinData)),
+                title='Summary of '+coinName+'('+coinSymbol+')',
+                input_message_content=InputTextMessageContent(
+                    "---"+coinName+" Summary"+'('+coinSymbol+')'+"---"+
+                    "\nPrice: "+coinPrice+
+                    "\nMarket Capitalization: "+coinCap+
+                    "\n1 hour percent change: "+coin1hr+
+                    "\n24 hour percent change: "+coin1day+
+                    "\n7 day percent change: "+coin7day),
                 thumb_url='https://i.imgur.com/t6BPcMR.png'
-            )
+            ),
         ]
     bot.answer_inline_query(update.inline_query.id, results)    
 
